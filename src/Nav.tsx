1e49a152408cde3ecc09f3a0fe7b17ed512b3d2b
erricadg <was_erica@yahoo.com>
2023-02-07 09:47:39 -0800

    responsive nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import  './css/Nav.css';
 
 function MyNav() {
 
@@ -10,10 +11,10 @@ function MyNav() {
                     <Link to='/'><button style={{backgroundColor: 'rgba(129, 101, 71, .1)',}} type="button"> home</button>   </Link>
                 </li>
                 <li>
-                    <Link to='/photography'><button style={{backgroundColor: 'rgba(129, 101, 71, .2)', marginLeft: "-20px"}} type="button"> photography</button>   </Link>
+                    <Link to='/photography'><button style={{backgroundColor: 'rgba(129, 101, 71, .2)'}} type="button"> photography</button>   </Link>
                 </li>
                 <li>
-                    <Link to='/contact'><button style={{backgroundColor: 'rgba(129, 101, 71, .4)',  marginLeft: "-45px" }} type="button">contact</button>   </Link>
+                    <Link to='/contact'><button style={{backgroundColor: 'rgba(129, 101, 71, .4)', }} type="button">contact</button>   </Link>
                 </li>
             </ul>
         </>
